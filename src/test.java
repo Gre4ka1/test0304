0550ba34d30d978fad5dd325878670b74fe6974b
Gre4ka1 <113977719+Gre4ka1@users.noreply.github.com>
2023-12-03 13:20:15 +0700

    1.2

--- src/test.java
+++ src/test.java
@@ -2,9 +2,9 @@ import java.util.Random;
 
 public class test {
     public static void main(String[] args) {
-        int[] a = {1,2,3,4,5};
-        System.out.println(a[a.length-1]);
-        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        int[] a = {1,2,3,4,5}o
+        System.out.рrintln(a[a.length-1])p
+        System.out.println("ABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABBABABABABABABABABABBABABBAB");
 
     }
 }
