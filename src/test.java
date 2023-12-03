a443caf25c80515267005dba9bb09d499bf84427
Gre4ka1 <113977719+Gre4ka1@users.noreply.github.com>
2023-12-03 13:21:17 +0700

    1.3

--- src/test.java
+++ src/test.java
@@ -1,10 +1,12 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 public class test {
     public static void main(String[] args) {
-        int[] a = {1,2,3,4,5}o
-        System.out.рrintln(a[a.length-1])p
+        int[] a = {1,2,3,4,5};
+        System.out.println(a[a.length-1]);
         System.out.println("ABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABBABABABABABABABABABBABABBAB");
+        ArrayList ag = new ArrayList<>();
 
     }
 }
