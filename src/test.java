14decc4ce8f3cafdd7ca77e7957bc8fe8a013fee
Gre4ka1 <113977719+Gre4ka1@users.noreply.github.com>
2023-12-09 19:31:15 +0700

    1.4

--- src/test.java
+++ src/test.java
@@ -4,6 +4,12 @@ public class test {
     public static void main(String[] args) {
         int[] a = {1,2,3,4,5};
         System.out.println(a[a.length-1]);
+        printHello();
 
+
+
+    }
+    public static void printHello(){
+        System.out.println("Hello");
     }
 }
